62830811bb48ac059d211b827a2f89d144e31ba1
Sankho1769 <banerjeeshubham00@gmail.com>
2025-08-11 19:25:31 +0530

    Add files via upload

---
+++ Anagram with counter.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+word1 = input("Enter first word: ")
+word2 = input("Enter second word: ")
+
+if Counter(word1) == Counter(word2):
+    print("Anagram")
+else:
+    print("Not anagram")
